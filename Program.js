b3863ee1df7c0f15e5153fa371b673f693042637
FizzyFriday <135688088+FizzyFriday@users.noreply.github.com>
2023-12-06 16:58:52 +1030

    Update Program.js

--- Program.js
+++ Program.js
@@ -1,16 +1,11 @@
-console.log("Running")
 const height = document.body.offsetHeight, width = document.body.offsetWidth;
-console.log(height);
-console.log(width);
+const AllGames = ["Game1", "Game2", "Game3"];
 
-
-var div = document.getElementById('hi');
-var divs = div.getElementsByTagName('div');
-var divArray = [];
-for (var i = 0; i < divs.length; i += 1) {
-  divArray.push(divs[i].innerHTML);
-  divArray[i].style.width = `${width*0.2933}px`;
-  divArray[i].style.height = `${height*0.4}px`;
-  divArray[i].style.left = `${width*0.03}px`;
-  divArray[i].style.right = `${width*0.03}px`;
+for (let i = 0; i < AllGames.length; i++)
+{
+  var div = document.getElementById(`${AllGames[i]}`);
+  div.style.left = `${height*0.03}px`;
+  div.style.right = `${height*0.03}px`;
+  div.style.width = `${height*0.2933}px`;
+  div.style.height = `${height*0.35}px`;
 }
