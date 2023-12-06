df5ef1f97d5ccfbad5608ca7b9c014c57a42431a
FizzyFriday <135688088+FizzyFriday@users.noreply.github.com>
2023-12-06 16:20:23 +1030

    Create Program.js

---
+++ Program.js
@@ -0,0 +1,12 @@
+const height = document.body.offsetHeight, width = document.body.offsetWidth;
+
+var div = document.getElementById('hi');
+var divs = div.getElementsByTagName('div');
+var divArray = [];
+for (var i = 0; i < divs.length; i += 1) {
+  divArray.push(divs[i].innerHTML);
+  divArray[i].style.width = `${width*0.2933}px`;
+  divArray[i].style.height = `${height*0.4}px`;
+  divArray[i].style.left = `${width*0.03}px`;
+  divArray[i].style.right = `${width*0.03}px`;
+}
