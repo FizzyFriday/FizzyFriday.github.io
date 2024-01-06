8da7caf56171181d7163a4ba412719e040d8c925
FizzyFriday <135688088+FizzyFriday@users.noreply.github.com>
2024-01-06 21:31:49 +1030

    Update Program.js

--- Program.js
+++ Program.js
@@ -1,11 +1,16 @@
-const height = document.body.offsetHeight, width = document.body.offsetWidth;
+const height = document.body.offsetHeight, width = document.body.offsetWidth
 
-for (let i = 1; i < 4; i++)
+var i = 1;
+while (true)
 {
+  try {var div = document.getElementById(`Div${i}`);
+  catch {break;}
+
   var div = document.getElementById(`Div${i}`);
   div.style.left = `${width*0.03}px`;
   div.style.right = `${width*0.03}px`;
-  div.style.width = `${height*0.2933}px`;
+  div.style.width = `${width*0.2933}px`;
   div.style.height = `${height*0.45}px`;
   div.style.marginBottom = "20px";
+  i++;
 }
