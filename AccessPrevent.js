6711a23b5d0c9d6184eb33331b4bd3a26b5e5e68
FizzyFriday <135688088+FizzyFriday@users.noreply.github.com>
2024-10-12 00:27:43 +1030

    Update AccessPrevent.js

--- AccessPrevent.js
+++ AccessPrevent.js
@@ -1,2 +1,2 @@
-alert("Due to games being downloaded being unable to update, resulting in a poor updating system, this page/the games are temporarily unaccessible. There is something new coming to deal with this")
+alert("Due to games being downloaded being unable to update, this page/the games are temporarily unaccessible. There is something new coming to make updating super simple.");
 window.onload = function() {document.body.innerHTML = '';}
